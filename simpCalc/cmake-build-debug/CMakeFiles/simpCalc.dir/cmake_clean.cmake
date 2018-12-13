5b3a818276a9211c9cbbd9ce23e0fd413cd4625f
Matthias <matthias.attard.9@gmail.com>
2018-12-13 11:03:43 +0100

    Created simple calculator program

---
+++ simpCalc/cmake-build-debug/CMakeFiles/simpCalc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpCalc.dir/main.c.obj"
+  "simpCalc.pdb"
+  "simpCalc.exe"
+  "simpCalc.exe.manifest"
+  "libsimpCalc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simpCalc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ simpCalc/main.c
@@ -0,0 +1,40 @@
+// Performs addition, subtraction, multiplication or division depending the input from user
+
+# include <stdio.h>
+
+int main() {
+
+    char oper;
+    int firstNum,secondNum;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &oper);
+
+    printf("Enter two integers: ");
+    scanf("%d %d",&firstNum, &secondNum);
+
+    switch(oper)
+            {
+                    case '+':
+                    printf("%d + %d = %d",firstNum, secondNum, firstNum + secondNum);
+                    break;
+
+                    case '-':
+                    printf("%d - %d = %d",firstNum, secondNum, firstNum - secondNum);
+                    break;
+
+                    case '*':
+                    printf("%d * %d = %d",firstNum, secondNum, firstNum * secondNum);
+                    break;
+
+                    case '/':
+                    printf("%d / %d = %d",firstNum, secondNum, firstNum / secondNum);
+                    break;
+
+                    // operator doesn't match any case constant (+, -, *, /)
+                    default:
+                    printf("Error! oper is not correct");
+            }
+
+        return 0;
+}
